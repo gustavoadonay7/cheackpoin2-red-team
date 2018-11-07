c464c631dc013814a5e20eb450f6666afe515499
Adonay <ddragonay@gmail.com>
2018-11-06 22:59:55 -0600

    State, Reducers, Store structure for users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import store from './state/store/store';
+import { Provider } from 'react-redux';
+import Users from './components/Users/Users';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Users />
+        </div>
+      </Provider>
     );
   }
 }
